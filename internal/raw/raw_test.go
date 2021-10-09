29829621bbf03a6590a9588a2df748a2ef404799
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-10-09 12:35:08 +0300

    decrease flood seconds

--- internal/raw/raw_test.go
+++ internal/raw/raw_test.go
@@ -18,13 +18,13 @@ func TestStartFlooding(t *testing.T) {
 		srcIp, dstIp                    string
 		srcMacAddr, dstMacAddr          []byte
 	}{
-		{"500byte", 500, srcPorts[rand.Intn(len(srcPorts))], 443, 2,
+		{"500byte", 500, srcPorts[rand.Intn(len(srcPorts))], 443, 1,
 			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
 			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
-		{"1000byte", 1000, srcPorts[rand.Intn(len(srcPorts))], 443, 2,
+		{"1000byte", 1000, srcPorts[rand.Intn(len(srcPorts))], 443, 1,
 			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
 			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
-		{"1400byte", 1400, srcPorts[rand.Intn(len(srcPorts))], 443, 2,
+		{"1400byte", 1400, srcPorts[rand.Intn(len(srcPorts))], 443, 1,
 			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
 			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
 	}
