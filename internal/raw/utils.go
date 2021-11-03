882156b357d6101e51b9f0bd8b1e2e2f0868fb77
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-11-03 22:07:33 +0300

    dummy commit to test if everything still works

--- internal/raw/utils.go
+++ internal/raw/utils.go
@@ -40,9 +40,6 @@ func getMacAddrs() [][]byte {
 			continue
 		}
 		macAddrs = append(macAddrs, buf)
-
-		//macAddrs = append(macAddrs, fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", buf[0], buf[1], buf[2], buf[3],
-		//	buf[4], buf[5], buf[6], buf[7]))
 	}
 
 	return macAddrs
