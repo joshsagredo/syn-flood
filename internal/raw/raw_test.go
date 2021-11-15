8e66c483e075d7fbe8b5aa0ea81eca2eb7f4e22e
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-11-15 20:15:53 +0300

    change cases

--- internal/raw/raw_test.go
+++ internal/raw/raw_test.go
@@ -22,10 +22,7 @@ func TestStartFlooding(t *testing.T) {
 		{"500byte", 500, srcPorts[rand.Intn(len(srcPorts))], 443, 500,
 			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
 			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
-		{"1000byte", 1000, srcPorts[rand.Intn(len(srcPorts))], 443, 500,
-			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
-			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
-		{"1400byte", 1400, srcPorts[rand.Intn(len(srcPorts))], 443, 500,
+		{"800byte", 1000, srcPorts[rand.Intn(len(srcPorts))], 443, 500,
 			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
 			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
 	}
