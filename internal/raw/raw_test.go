64744691cf1904ea6d12b1942df2dbe35b724d28
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-11-21 20:25:37 +0300

    remove 3rd case

--- internal/raw/raw_test.go
+++ internal/raw/raw_test.go
@@ -18,10 +18,10 @@ func TestStartFlooding(t *testing.T) {
 		srcIp, dstIp                    string
 		srcMacAddr, dstMacAddr          []byte
 	}{
-		{"500byte_syn", "syn", 500, srcPorts[rand.Intn(len(srcPorts))], 443, 100,
+		{"500byte_syn", "syn", 500, srcPorts[rand.Intn(len(srcPorts))], 443, 50,
 			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
 			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
-		{"500byte_ack", "ack", 500, srcPorts[rand.Intn(len(srcPorts))], 443, 100,
+		{"500byte_ack", "ack", 500, srcPorts[rand.Intn(len(srcPorts))], 443, 50,
 			srcIps[rand.Intn(len(srcIps))], "213.238.175.187",
 			macAddrs[rand.Intn(len(macAddrs))], macAddrs[rand.Intn(len(macAddrs))]},
 	}
